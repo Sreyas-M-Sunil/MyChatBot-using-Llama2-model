bbe0bfe84e4a1e3a590176aa0ba41b99d2d7345b
Sreyas-M-Sunil <sreyasmsofficial@gmail.com>
2025-08-15 16:35:01 +0530

    Updations

---
+++ src/helper.py
@@ -0,0 +1,26 @@
+from langchain.document_loaders import  PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+#Loading data 
+def load_doc(file_path):
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+    return documents
+
+#Splitting the documents
+def split_doc(extracted_data):
+    split = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap=20)
+    text_chunks = split.split_documents(extracted_data)
+    return text_chunks
+
+#Accessing the text_chunks
+def get_text_chunk():
+    text_chunk = split_doc(extracted_data)
+    return text_chunk
+
+#Download the embedding model
+def download_embedding():
+    embeddings = HuggingFaceEmbeddings(model_name ="sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
+
