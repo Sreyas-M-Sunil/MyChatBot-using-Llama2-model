38d4e1fc1b6fa0712cb0037536669f78bedc3c84
Sreyas-M-Sunil <sreyasmsofficial@gmail.com>
2025-08-15 15:50:05 +0530

    Files created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'MyChatbot',
+    version = '0.0.0',
+    author = 'Sreyas M Sunil',
+    packages= find_packages(),
+    install_requires = []
+)
